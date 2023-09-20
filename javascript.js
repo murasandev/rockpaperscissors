55daf7b507a4bfaf60d8360405158ec92b7106e8
Dylan <dmurayama93@gmail.com>
2023-09-19 20:25:45 -1000

    break problem down into pseudocode

---
+++ javascript.js
@@ -0,0 +1,11 @@
+// Goal: Create a rock paper scissors game against the computer
+// First to 5, wins the game
+
+// Write function to play a single round of rps.
+// function takes two parameters (player selection, computer selection)
+// return/declare player and computer selections
+// return/declare winner of the round
+
+// Run game function through a loop until player or computer reach 5 wins
+// After each round add score to round winners total score.
+// Once player or computer reach a score of 5, end game
